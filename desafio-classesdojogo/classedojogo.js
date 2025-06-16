3fd5a6295fc169c6d91d16f0b0589b37e3181e1f
rhommardev2023 <rjmlp02@gmail.com>
2025-06-15 22:40:01 -0700

    usando switch case

--- desafio-classesdojogo/classedojogo.js
+++ desafio-classesdojogo/classedojogo.js
@@ -7,5 +7,25 @@ class Heroi {
     this.tipo =tipo;
     }
 
-    
+    atacar() {
+      let ataque;
+      switch (this.tipo) {
+        case 'mago':
+          ataque = 'magia';
+          break;
+        case 'guerreiro':
+          ataque = 'espada';
+          break;
+        case 'monge':
+          ataque = 'artes marciais';
+          break;
+
+          case 'ninja':
+            ataque = 'shuriken';
+            break;
+        default:
+          ataque = 'Ataque desconhecido';
+      }
+    }
+
 }
